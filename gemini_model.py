c45091ad9578f1ac1583cc07d30960c87dae52fb
Tapoban Ray <raytapoban@gmail.com>
2025-04-23 23:33:06 +0530

    Integrated the LLM with Streamlit UI

---
+++ gemini_model.py
@@ -0,0 +1,42 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from env_enums import ENV_ENUMS
+
+
+def generate_output(
+    prompt: str,
+    temperature: float = 1.5,
+    top_p: float = None,
+    top_k: float = None,
+    max_output_tokens: int = 10,
+):
+    print("Temperature:", temperature)
+    print("max output token:", max_output_tokens)
+    print("top_k:", top_k)
+    print("top_p:", top_p)
+    llm = ChatGoogleGenerativeAI(
+        model=ENV_ENUMS.GEMINI_LLM_MODEL,
+        api_key=ENV_ENUMS.GEMINI_API_KEY,
+        max_output_tokens=max_output_tokens,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+    )
+
+    output = llm.invoke(prompt)
+
+    return output.content
+
+
+def get_prompt(human_message: str, system_message: str, chat_history: list):
+    prompt_template = ChatPromptTemplate(
+        [
+            ("system", system_message),
+            (MessagesPlaceholder(variable_name="chat_history")),
+            ("human", "{query}"),
+        ]
+    )
+
+    return prompt_template.invoke(
+        {"chat_history": chat_history, "query": human_message}
+    )
--- main.py
+++ main.py
@@ -1,32 +1,85 @@
 import streamlit as st
+from gemini_model import generate_output, get_prompt
 
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+TOP_P = "top_p"
+TOP_K = "top_k"
+TEMPERATURE = "temperature"
+MAX_OUTPUT_TOKENS = "max_output_tokens"
+CHAT_HISTORY = "chat_history"
+SYSTEM_MESSAGE = "system_message"
+
+session_variables = {
+    CHAT_HISTORY: [],
+    TOP_K: 0.0,
+    TOP_P: 0.0,
+    MAX_OUTPUT_TOKENS: 0,
+    TEMPERATURE: 0.0,
+    SYSTEM_MESSAGE: "You are a helpful assistant.",
+}
+
+for var in session_variables.keys():
+    if var not in st.session_state:
+        st.session_state[var] = session_variables.get(var)
+
+
+@st.fragment()
+def sidebar_content():
+    st.session_state[SYSTEM_MESSAGE] = st.text_area(
+        "Enter System Message...", value=st.session_state[SYSTEM_MESSAGE]
+    )
+
+    st.title(body="Parameters")
+    st.session_state[TEMPERATURE] = st.slider(
+        label="Temperature",
+        min_value=0.0,
+        max_value=2.0,
+        value=1.5,
+    )
+    st.session_state[TOP_P] = st.slider(
+        label="Top_p", min_value=0.0, max_value=1.0, value=1.0
+    )
+    st.session_state[TOP_K] = st.number_input(label="Top_k", min_value=1, value=1)
+    st.session_state[MAX_OUTPUT_TOKENS] = st.number_input(
+        label="Max Output Tokens", min_value=1, value=1024
+    )
+
+
+st.title("AI Chatbot")
+
+st.chat_message("ai").markdown(generate_output(prompt="Say hi to user in order to initiate conversation."))
 
 with st.sidebar:
-    st.slider(label="Temperature", min_value=0.0, max_value=2.0, )
+    sidebar_content()
 
 
-def append_to_chat_history():
-    print("Hello")
-    ai_message = "Hey Human"
-    st.session_state.chat_history.append({"human": prompt})
-    st.session_state.chat_history.append({"ai": ai_message})
+def append_to_chat_history(prompt: str):
+    final_prompt = get_prompt(
+        human_message=prompt,
+        chat_history=st.session_state[CHAT_HISTORY],
+        system_message=st.session_state[SYSTEM_MESSAGE],
+    )
+    ai_message = generate_output(
+        prompt=final_prompt,
+        max_output_tokens=st.session_state[MAX_OUTPUT_TOKENS],
+        temperature=st.session_state[TEMPERATURE],
+        top_k=st.session_state[TOP_K],
+        top_p=st.session_state[TOP_P],
+    )
+    st.session_state[CHAT_HISTORY].append(("human", prompt))
+    st.session_state[CHAT_HISTORY].append(("ai", ai_message))
 
-    chat: dict
-    print(st.session_state.chat_history)
-
-    for chat in st.session_state.chat_history:
-        if chat.get("ai") is not None:
-            with st.chat_message("ai"):
-                st.write(chat.get("ai"))
-        else:
-            with st.chat_message("user"):
-                st.write(chat.get("human"))
+    st.chat_message("ai").markdown(ai_message)
 
 
-prompt = st.chat_input(placeholder="Enter your message...")
+# display chat history
+for chat in st.session_state[CHAT_HISTORY]:
+    st.chat_message(chat[0]).write(chat[1])
 
-if prompt is not None:
-    append_to_chat_history()
+user_prompt = st.chat_input(
+    placeholder="Enter your message...",
+)
+
+if user_prompt:
+    st.chat_message("human").write(user_prompt)
+    append_to_chat_history(prompt=user_prompt)
